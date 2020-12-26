cc3286b39db289f6e2bf347de5d4da8977e27c12
KORguy <hyun.kang@hotmail.com>
2020-12-26 18:46:06 +0900

    .

--- src/Components/Transition.js
+++ src/Components/Transition.js
@@ -1,28 +1,47 @@
-import React, { useEffect, useRef } from "react";
+// import React, { useEffect, useRef } from "react";
+
+// export const Transition = ({ duration, setTransition }) => {
+//   var count = duration;
+//   const cntRef = useRef(count);
+//   let cntContainer;
+
+//   useEffect(() => {
+//     cntContainer = document.getElementById("cnt");
+//     const temp = () => {
+//       cntRef.current -= 1;
+//       cntContainer.innerHTML = cntRef.current;
+//       setTimeout(() => {
+//         temp();
+//       }, 1000);
+//       if (cntRef.current === 0) {
+//         setTransition(false);
+//       }
+//     };
+//     temp();
+//   });
+
+//   return (
+//     <div className="loading">
+//       <h1 id="cnt"></h1>
+//     </div>
+//   );
+// };
+
+import React, { useEffect, useState } from "react";
 
 export const Transition = ({ duration, setTransition }) => {
-  var count = duration;
-  const cntRef = useRef(count);
-  let cntContainer;
-
+  const [count, setCount] = useState(duration);
   useEffect(() => {
-    cntContainer = document.getElementById("cnt");
-    const temp = () => {
-      cntRef.current -= 1;
-      cntContainer.innerHTML = cntRef.current;
-      setTimeout(() => {
-        temp();
-      }, 1000);
-      if (cntRef.current === 0) {
-        setTransition(false);
-      }
-    };
-    temp();
+    setTimeout(() => {
+      setCount(count - 1);
+    }, 1000);
+    if (count === 0) {
+      setTransition(false);
+    }
   });
-
   return (
     <div className="loading">
-      <h1 id="cnt"></h1>
+      <h1>{count}</h1>
     </div>
   );
 };
